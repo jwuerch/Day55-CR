7045bf30cb80ef74a4dee777c96ee77e5d69ef90
Jason Wuerch <wuerchjason@gmail.com>
2016-03-18 11:07:30 -0700

    successfully call specific response for specific usernames in browser-interface.js

--- build/js/app.js
+++ build/js/app.js
@@ -5,10 +5,14 @@ exports.apiKey = "baf63b0862f709be303d3bd6c3ab542f49f811ab";
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
+  $('.search-btn').click(function() {
+    var username = $('#user-name').val();
+    console.log($('#user-name').val());
+    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+      console.log(response);
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
   });
 });
 
--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,9 +1,13 @@
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
+  $('.search-btn').click(function() {
+    var username = $('#user-name').val();
+    console.log($('#user-name').val());
+    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+      console.log(response);
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
   });
 });

b37aaaf5ec810cf3dde0e77960ec50eb21be8510
Jason Wuerch <wuerchjason@gmail.com>
2016-03-18 10:43:19 -0700

    add in functionality in browser-interface.js to console log API information

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,5 +1,9 @@
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  
-})
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+});

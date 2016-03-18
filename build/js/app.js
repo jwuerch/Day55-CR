aaf0d11b31625478e159b3b72252c32573548c71
Jason Wuerch <wuerchjason@gmail.com>
2016-03-18 10:55:09 -0700

    fix errors to successfully call apiKey in browser-interface.js

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,15 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "baf63b0862f709be303d3bd6c3ab542f49f811ab";
 
-},{}]},{},[1]);
+},{}],2:[function(require,module,exports){
+var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function() {
+  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+});
+
+},{"./../.env":1}]},{},[2]);

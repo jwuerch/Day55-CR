9f42d7992ba4efc9c75ec2bee9d264eb5a9486e0
Jason Wuerch <wuerchjason@gmail.com>
2016-03-18 11:11:53 -0700

    calls complete list of user's repositories

--- build/js/app.js
+++ build/js/app.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
   $('.search-btn').click(function() {
     var username = $('#user-name').val();
     console.log($('#user-name').val());
-    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-      console.log(response);
+    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+      console.log(JSON.stringify(response));
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });

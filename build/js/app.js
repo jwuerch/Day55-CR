8497861a7c5b87c0f84744916085b8dd3d4cf917
Jason Wuerch <wuerchjason@gmail.com>
2016-03-18 12:11:23 -0700

    include description of each repository

--- build/js/app.js
+++ build/js/app.js
@@ -26,20 +26,20 @@ $(document).ready(function() {
       console.log(response);
       getRepos(response).forEach(function(element){
         console.log(element.name);
-        $('.results-box').append('<p><a href="https://github.com/' + username + '/' + element.name + '">' + element.name + '</a> - ' + moment(element.pushed_at).format('MMMM Do YYYY') + '</p>');
+        $('.results-box').append('<p><a href="https://github.com/' + username + '/' + element.name + '">' + element.name + '</a> - ' + element.description + '<p><li><span style="text-decoration:underline;">Date Created</span>: ' +  moment(element.pushed_at).format('MMMM Do YYYY') + '</li></p>');
       });
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
 
     $('.results-box-two').pagination({
-      totalPage: 10,
+      totalPage: 5,
       callback: function(currentPage){
 
         $.get('https://api.github.com/users/' + username + '/repos?page=' + currentPage + '&per_page=30').then(function(response){
           $('.results-box').empty();
           getRepos(response).forEach(function(element){
-            $('.results-box').append('<p><a href="https://github.com/' + username + '/' + element.name + '">' + element.name + '</a> - ' + moment(element.pushed_at).format('MMMM Do YYYY') + '</p>');
+            $('.results-box').append('<p><a href="https://github.com/' + username + '/' + element.name + '">' + element.name + '</a> - ' + element.description + '<p><li><span style="text-decoration:underline;">Date Created</span>: ' +  moment(element.pushed_at).format('MMMM Do YYYY') + '</p>');
           });
         }).fail(function(error){
           console.log(error.responseJSON.message);
--- js/browser-interface.js
+++ js/browser-interface.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
       console.log(response);
       getRepos(response).forEach(function(element){
         console.log(element.name);
-        $('.results-box').append('<p><a href="https://github.com/' + username + '/' + element.name + '">' + element.name + '</a> - ' + moment(element.pushed_at).format('MMMM Do YYYY') + '</p>');
+        $('.results-box').append('<p><a href="https://github.com/' + username + '/' + element.name + '">' + element.name + '</a> - ' + element.description + '<p><li><span style="text-decoration:underline;">Date Created</span>: ' +  moment(element.pushed_at).format('MMMM Do YYYY') + '</li></p>');
       });
     }).fail(function(error){
       console.log(error.responseJSON.message);
@@ -22,7 +22,7 @@ $(document).ready(function() {
         $.get('https://api.github.com/users/' + username + '/repos?page=' + currentPage + '&per_page=30').then(function(response){
           $('.results-box').empty();
           getRepos(response).forEach(function(element){
-            $('.results-box').append('<p><a href="https://github.com/' + username + '/' + element.name + '">' + element.name + '</a> - ' + moment(element.pushed_at).format('MMMM Do YYYY') + '</p>');
+            $('.results-box').append('<p><a href="https://github.com/' + username + '/' + element.name + '">' + element.name + '</a> - ' + element.description + '<p><li><span style="text-decoration:underline;">Date Created</span>: ' +  moment(element.pushed_at).format('MMMM Do YYYY') + '</p>');
           });
         }).fail(function(error){
           console.log(error.responseJSON.message);
